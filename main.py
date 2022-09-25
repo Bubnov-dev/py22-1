0ce6fcaf190066788d2eaf6af50feb91bc0598ef
sasha.bubnov.k@gmail.com <sasha.bubnov.k@gmail.com>
2022-09-25 14:02:04 +0300

    format

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from random import randrange
+
 file = []
 tags = []
 with open('books.csv', 'r', newline='', encoding='windows-1251') as csvfile:
@@ -9,36 +10,39 @@ with open('books.csv', 'r', newline='', encoding='windows-1251') as csvfile:
     book_tags = []
     for row in list(rows)[1:]:
         file.append(row)
-        count = count +1
-        book_tags = row[len(row)-1].split('# ')
+        count = count + 1
+        book_tags = row[len(row) - 1].split('# ')
         tags += book_tags
-        if len(row[1])>30:
-            long_names=long_names+1
-    print('длинных имен: ' +    str(long_names))
+        if len(row[1]) > 30:
+            long_names = long_names + 1
+    print('длинных имен: ' + str(long_names))
 
 print('tags: ')
 print(set(tags))
 print('записей: ' + str(len(file)))
 
-start = randrange(len(file)-20)
+start = randrange(len(file) - 20)
 f = open('text.txt', 'w')
 j = 1
-for i in range(start, start+20):
+for i in range(start, start + 20):
     f.write(str(j) + '. ' + file[i][3] + '. ' + file[i][1] + ' - ' + file[i][6] + '\n')
-    j = j+1
+    j = j + 1
+
 
 def get_issues(el):
     return el[8]
 
+
 by_popular = file.copy()
-by_popular.sort(reverse=True, key = get_issues)
+by_popular.sort(reverse=True, key=get_issues)
 for i in range(0, 19):
     print(by_popular[i])
 
 search = input('введите поисковый запрос')
 
+
 def checkBook(book):
-    return search.lower() in book[3].lower() and float(book[7].replace(',', '.'))>200
+    return search.lower() in book[3].lower() and float(book[7].replace(',', '.')) > 200
+
 
 print(list(filter(checkBook, file)))
-
